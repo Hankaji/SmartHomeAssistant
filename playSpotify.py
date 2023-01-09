b66270542ebb8c57dc3e63d55f8de4b8b6f4f710
Rainb0wBanana <s3977773@rmit.edu.vn>
2023-01-09 15:45:35 +0700

    Completed the search and play function

--- playSpotify.py
+++ playSpotify.py
@@ -25,12 +25,14 @@ while True:
             sp.start_playback(res["devices"][0]["id"])
         except:
             print("Already playing!")
+    
     elif command == "playing":
         try:
             playback = sp.current_playback()
             print("Now playing", playback['item']['name'], "-", playback['item']['artists'][0]['name'])
         except:
             print("Nothing is playing!")
+    
     elif command == "stop":
         try:
             res = sp.devices()
@@ -40,34 +42,58 @@ while True:
             sp.pause_playback(res["devices"][0]["id"])
         except:
             print("Nothing is playing!")
+    
     elif command == "next":
         res = sp.devices()
 
         print("Skipping to next song on device:",res["devices"][0]["name"])
 
         sp.next_track(res["devices"][0]["id"])
+    
     elif command == "previous":
         res = sp.devices()
 
         print("Reverting to previous song on device:",res["devices"][0]["name"])
 
         sp.previous_track(res["devices"][0]["id"])
+    
     elif command == "volume up":
         res = sp.devices()
-        print(res['devices'][0]['volume_percent'])
-        sp.volume(res['devices'][0]['volume_percent'] + 10, res["devices"][0]["id"])
+        volume = res['devices'][0]['volume_percent']
+        sp.volume(volume + 10, res["devices"][0]["id"])
         res = sp.devices()
-        print(res['devices'][0]['volume_percent'])
+        print("Increased volume to", (volume + 10))
+    
     elif command == "volume down":
         res = sp.devices()
-        print(res['devices'][0]['volume_percent'])
-        sp.volume(res['devices'][0]['volume_percent'] - 10, res["devices"][0]["id"])
+        volume = res['devices'][0]['volume_percent']
+        sp.volume(volume - 10, res["devices"][0]["id"])
         res = sp.devices()
-        print(res['devices'][0]['volume_percent'])
+        print("Decreased volume to", (volume - 10))
+    
     elif command == "search":
         a = input("Song name: ")
-        res = sp.search(a, 1, 0,"track")
-        print(res['tracks']["items"][0]["external_urls"]["spotify"])
+        dev = sp.devices()
+        device = dev["devices"][0]["id"]
+        res = sp.search(a, limit = "1")
+        song_res = res["tracks"]["items"][0]
+
+        # if song_res["is_playable"] != True:
+        #     print("Song is not playable!")
+        # else:
+        result = {"artist": "", "song": "", "uri": ""}
+        artist = ""
+        if len(song_res["artists"]) > 1:
+            artist_list = ([x["name"] for x in song_res["artists"]])
+            artist = " and "
+            artist = artist.join(artist_list)
+        else:
+            artist = song_res["artists"][0]["name"]
+        result["uri"] = song_res["uri"]
+        result["song"] = song_res["name"]
+        result["artist"] = artist
+        print("Playing %s by %s!"%(result["song"], result["artist"]))
+        sp.start_playback(uris = [result["uri"]])
 
     elif command == "s":
         break
